4e1016171960bc50b9abd6494dbc1e9d5306606d
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-05-30 18:21:34 +0200

    homepage

--- app.js
+++ app.js
@@ -28,8 +28,11 @@ function goToTodoPage(id, name) {
 
 function populateTagContainer(container, tags){
   for (const tag of tags) {
-    const span = document.createElement('span');
-    span.classList.add('chip');
+    const span = document.createElement('span'); 
+    span.classList.add('badge'); 
+    span.classList.add('rounded-pill');
+    span.classList.add('text-bg-primary'); 
+    span.classList.add('chip'); 
     const node = document.createTextNode('#' + tag);
     span.appendChild(node);
     container.appendChild(span)
@@ -39,16 +42,43 @@ function populateTagContainer(container, tags){
 
 function createTodoCard(todo){
 
-  const cardTemplate = `
-      <span class="todo-name">#NAME</span>
-      <div class="tag-container"></div>
-      <span>#CREATIONDATE</span>
-      <div class="divider"></div>
-      <div class="buttons-container">
-        <button class="delete-button"><img width="20px" src="./assets/delete.svg" alt=""></button>
-        <button class="edit-button"><img width="20px" src="./assets/edit.svg" alt=""></button>
-        <button class="done-button"><img width="20px" src="./assets/check.svg" alt=""></button>
-        </div>`
+  const cardTemplate = 
+    // `
+    //   <span class="todo-name">#NAME</span>
+    //   <div class="tag-container"></div>
+    //   <span>#CREATIONDATE</span>
+    //   <div class="divider"></div>
+    //   <div class="buttons-container">
+    //     <button class="delete-button"><img width="20px" src="./assets/delete.svg" alt=""></button>
+    //     <button class="edit-button"><img width="20px" src="./assets/edit.svg" alt=""></button>
+    //     <button class="done-button"><img width="20px" src="./assets/check.svg" alt=""></button>
+    //     </div> 
+    // ` 
+    ` 
+    <div class="card divider todo-card" style="width: 90%;">
+      <div class="card-body">
+        <h5 class="card-title">#NAME</h5> 
+        <span>#CREATIONDATE</span>
+        <div class="card-text tag-container"></div> 
+        <button class="btn btn-success delete-button"> 
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+          </svg> 
+        </button> 
+        <button class="btn btn-success edit-button"> 
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+          <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+          </svg>
+        </button> 
+        <button class="btn btn-success done-button"> 
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+          <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+          </svg>
+        </button> 
+      </div>
+    </div>
+    `
   
   
   //const humanDate = new Date(todo.creationDate * 1000)
